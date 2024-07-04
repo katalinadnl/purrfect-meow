08edfa04d795d32c9a1c0da4be1a7e95ed9d40e6
Catalina DANILA <catalinadanila6@gmail.com>
2024-07-04 11:51:17 +0200

    add roles

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,10 +35,13 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $isFirstUser = User::count() === 0;
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'roles' => $isFirstUser ? User::ROLE_ADMIN : User::ROLE_CLIENT,
         ]);
 
         event(new Registered($user));
